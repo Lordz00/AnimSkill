5a8a480a707e2a31f4626c13dafa6b1539c5bc52
Lordz00 <66584487+Lordz00@users.noreply.github.com>
2021-08-06 10:57:26 +0200

    finished

---
+++ javaSkill.js
@@ -0,0 +1,332 @@
+// ENEMY
+
+
+// function blank() {
+
+//   document.getElementById("myImgEnemy").src="blank";
+
+// }
+
+//WOJOWNIK
+function blyskAtak() {
+
+  document.getElementById("myImgEnemy").src="img/woj/blyskawiczny-atak.gif";
+
+}
+
+function niszCios() {
+
+  document.getElementById("myImgEnemy").src="img/woj/niszczycielskiCios.gif";
+
+}
+
+function attTar() {
+
+  document.getElementById("myImgEnemy").src="img/woj/atak-zza-tarczy.gif";
+
+}
+
+
+function oglusz() {
+
+  document.getElementById("myImgEnemy").src="img/woj/ogluszenie.gif";
+
+}
+
+function taunt() {
+
+  document.getElementById("myImgSelf").src="img/taunt.gif";
+
+}
+
+function oslTar() {
+
+  document.getElementById("myImgSelf").src="img/woj/oslona-tarcza.gif";
+
+}
+
+function krwioSzal() {
+
+  document.getElementById("myImgSelf").src="img/woj/krwiozerczy-szal.gif";
+
+}
+
+function agrAtt() {
+
+  document.getElementById("myImgEnemy").src="img/woj/agresywny-atak.gif";
+
+}
+
+function szarza() {
+
+  document.getElementById("myImgSelf").src="img/woj/szarza.gif";
+
+}
+
+function eneRegen() {
+
+  document.getElementById("myImgSelf").src="img/univ/energyRegen.gif";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+
+
+
+
+//LOWCA
+function blyskStrzal() {
+
+    document.getElementById("myImgEnemy").src="img/low/blyskawiczny-strzal.gif";
+  
+  }
+
+
+function podwStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/low/podwojny-strzal.gif";
+
+}
+
+function zatStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/low/zatruta-strzala.gif";
+
+}
+
+function wynRan() {
+
+  document.getElementById("myImgEnemy").src="img/low/wyniszczajce-rany.gif";
+
+}
+
+function deszczStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/low/deszcz-strzal.gif";
+
+}
+
+function heal() {
+
+  document.getElementById("myImgAlly").src="img/heal.gif";
+
+}
+
+function restoGreen() {
+
+  document.getElementById("myImgSelf").src="img/univ/restorationGreen.gif";
+  document.getElementById("myImgAlly").src="img/univ/restorationGreen.gif";
+
+
+}
+
+function saBoost() {
+
+  document.getElementById("myImgSelf").src="img/univ/saBoost2.gif";
+  document.getElementById("myImgAlly").src="img/univ/saBoost2.gif";
+
+
+}
+
+// MAG
+
+function fireBall() {
+
+  document.getElementById("myImgEnemy").src="img/mag/kula-ognia.gif";
+
+}
+
+function porazenie() {
+
+  document.getElementById("myImgEnemy").src="img/mag/porazenie.gif";
+
+}
+
+function lodPoc() {
+
+  document.getElementById("myImgEnemy").src="img/mag/lodowy-pocisk.gif";
+
+}
+
+function fuzZyw() {
+
+  document.getElementById("myImgEnemy").src="img/mag/fuzja-zywiolow.gif";
+
+}
+
+function szadz() {
+
+  document.getElementById("myImgEnemy").src="img/mag/szadz.gif";
+
+}
+
+function manaReg() {
+
+  document.getElementById("myImgSelf").src="img/univ/manaRegen.gif";
+  document.getElementById("myImgAlly").src="img/univ/manaRegen.gif";
+
+}
+
+
+// PALADYN
+
+function fireAtt() {
+
+  document.getElementById("img/pal/gorace-uderzenie.gif").src="blank";
+
+}
+
+function lightStrike() {
+
+  document.getElementById("myImgEnemy").src="img/pal/porazajacy-cios.gif";
+
+}
+
+function iceStrike() {
+
+  document.getElementById("myImgEnemy").src="img/pal/pchniecie-mrozu.gif";
+
+}
+
+function gniewBog() {
+
+  document.getElementById("myImgEnemy").src="img/pal/gniew-bogow.gif";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+// TROPICIEL
+
+function ploStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/plonaca-strzala.gif";
+
+}
+
+function porStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/porazajaca-strzala.gif";
+
+}
+
+function lodStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/lodowa-strzala.gif";
+
+}
+
+function podwStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/podwojny-strzal.gif";
+
+}
+
+function supStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/strzala-z-niespodzianka.gif";
+
+}
+
+function wycStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/wycienczajaca-strzala.gif";
+
+}
+
+function gradStrzal() {
+
+  document.getElementById("myImgEnemy").src="img/trop/grad-strzal.gif";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+// TANC
+function truPchn() {
+
+  document.getElementById("myImgEnemy").src="img/tanc/trujace-pchniecie.gif";
+
+}
+
+function podUd() {
+
+  document.getElementById("myImgEnemy").src="img/tanc/podstepne-uderzenie.gif";
+
+}
+
+function rozpAta() {
+
+  document.getElementById("myImgEnemy").src="img/tanc/rozpraszajacy-atak.gif";
+
+}
+
+function rozjusz() {
+
+  document.getElementById("myImgEnemy").src="img/tanc/rozjuszenie.gif";
+
+}
+
+function debuffRed() {
+
+  document.getElementById("myImgEnemy").src="img/univ/debuffRed.gif";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+// OTHER
+
+function blok() {
+
+  document.getElementById("myImgAlly").src="img/blok.gif";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+function blank() {
+
+  document.getElementById("myImgEnemy").src="blank";
+
+}
+
+
+function edit() {
+
+    document.getElementById("myImgSelf").src="img/quma.gif";
+  
+  }
