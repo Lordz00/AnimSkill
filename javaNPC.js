73fd5c40e85db81434c892069d71238b89e5febd
Lordz00 <66584487+Lordz00@users.noreply.github.com>
2021-08-16 12:21:36 +0200

    publishNPC

---
+++ javaNPC.js
@@ -0,0 +1,327 @@
+function tooltipOn() {
+
+    document.getElementById("toolptip").style.visibility = "visible";
+  
+  }
+
+  function tooltipOff() {
+
+    document.getElementById("toolptip").style.visibility = "hidden";
+  
+  }
+
+
+
+
+function bgDef() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-def.png";
+  
+  }
+
+  function bgAltar() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-altar.png";
+  
+  }
+
+  function bgCaveMineral() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-caveMineral.png";
+  
+  }
+
+  
+  function bgCaveSand() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-caveSand.png";
+  
+  }
+
+  function bgDarkAltar() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-darkAltar.png";
+  
+  }
+
+  
+  function bgDarkOffice() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-darkOffice.png";
+  
+  }
+
+  
+  function bgDarkRoom() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-darkRoom.png";
+  
+  }
+
+  
+  function bgDarkTown() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-darkTown.png";
+  
+  }
+
+  
+  function bgSnowForest() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-snowForest.png";
+  
+  }
+
+  
+  function bgSnowWalls() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-snowWalls.png";
+  
+  }
+
+  
+  function bgDesert() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-desert.png";
+  
+  }
+
+  
+  function bgDesertRocks() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-desertRocks.png";
+  
+  }
+
+  
+  function bgForest() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-forest.png";
+  
+  }
+
+  
+  function bgInn() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-inn.png";
+  
+  }
+
+  
+
+
+  
+  function bgMahoRoom() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-mahoRoom.png";
+  
+  }
+
+  
+  function bgPrison() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-prison.png";
+  
+  }
+
+  
+  function bgShroomCave() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-shroomCave.png";
+  
+  }
+
+  
+  function bgSnow() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-snow.png";
+  
+  }
+
+  function bgTomb() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-tomb.png";
+  
+  }
+
+  function bgTown() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-town.png";
+  
+
+  }
+  function bgWalls() {
+
+    document.getElementById("testBg").src="imgNPC/bg/bg-walls.png";
+  
+  }
+
+//NPC
+function npcAuk() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/auk.gif";
+  
+  }
+
+
+  function npcAlrik() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/alrik.gif";
+  
+  }
+
+  function npcTp() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/tp.gif";
+  
+  }
+
+  function npcIth() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/ith.gif";
+  
+  }
+
+  function npc() {
+
+    document.getElementById("npcDummy").src="imgNPC";
+  
+  }
+
+  function npc() {
+
+    document.getElementById("npcDummy").src="imgNPC";
+  
+  }
+
+  function npcKwi() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/kwi.gif";
+  
+  }
+
+  function npcMei() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/mei.gif";
+  
+  }
+
+  function npcKon() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/kon.gif";
+  
+  }
+
+  function npcKult() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/kult.gif";
+  
+  }
+
+  function npcDuch() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/duch.gif";
+  
+  }
+
+
+  function npcKoch() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/koch.gif";
+  
+  }
+
+  function npcDuch() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/duch.gif";
+    // document.getElementById("demo").innerHTML = "Duch";
+
+  
+  }
+
+  function npcBaz() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/baz.gif";
+    // document.getElementById("demo").innerHTML = "Bazyliszek";
+    document.getElementById("testBubble").src="imgNPC/bg-none.gif";
+
+  
+  }
+
+  function npcDem() {
+
+    document.getElementById("npcDummy").src="imgNPC/npc/dem.gif";
+    document.getElementById("testBubble").src="imgNPC/bg-none.gif";
+
+  }
+
+
+
+//Bubbles
+
+function bgbNone() {
+
+    document.getElementById("testBubble").src="imgNPC/bg-none.gif";
+  
+  }
+
+
+function bgbA1() {
+
+    document.getElementById("testBubble").src="imgNPC/b-bg/bbg-A1.gif";
+  
+  }
+
+  
+function bgbA2() {
+
+    document.getElementById("testBubble").src="imgNPC/b-bg/bbg-A2.gif";
+  
+  }
+
+
+  
+function bgbA3() {
+
+    document.getElementById("testBubble").src="imgNPC/b-bg/bbg-A3.gif";
+  
+  }
+
+  
+function bgbA4() {
+
+    document.getElementById("testBubble").src="imgNPC/b-bg/bbg-A4.gif";
+  
+  }
+
+  
+function bgbA5() {
+
+    document.getElementById("testBubble").src="imgNPC/b-bg/bbg-A5.gif";
+  
+  }
+
+
+
+  
+//   function () {
+
+//     document.getElementById("testBg").src="imgNPC";
+  
+//   }
+
+  
+//   function () {
+
+//     document.getElementById("testBg").src="imgNPC";
+  
+//   }
+
+
+
+
+//   function () {
+
+//     document.getElementById("testBg").src="imgNPC";
+  
+//   }
