19886b25f60bb59f44f3409ba87e90208d899e43
Lordz00 <66584487+Lordz00@users.noreply.github.com>
2022-05-17 08:12:51 +0200

    Update canvas-script.js

--- resources/28 - save canvas as an image/canvas-script.js
+++ resources/28 - save canvas as an image/canvas-script.js
@@ -1,51 +1,62 @@
 
+    // template
+    function template() {
 
 
-window.onload = function () {
+      // template
+      let template = new Array();
 
-  // Definitions
-  var canvas = document.getElementById("save-canvas");
-  var context = canvas.getContext("2d");
+      // create new template entry
+      function templateAdd() {
+          let templateNum = template.length + 1;
+          template.push('itm' + templateNum);
 
-  // drawImage(img, dx, dy)
-  // drawImage(img, dx, dy, dw, dh)
-  // drawImage(img, sx, sy, sw, sh, dx, dy, dw, dh)
+          // draw new template entry
+          const templateDraw = () => {
+              ctx.fillStyle = itmColor;
+              varRow = templateNum;
+              itmHeight = 0;
+              lockAttCloth();
+              ctx.fillRect(templateNum * 32, itmHeight * 32, 32, 32);
+              drawDesc();
+          }
 
+          templateDraw();
+      };
 
-  var amsterdamImage = new Image();
-  amsterdamImage.src = "amsterdam.jpg";
-
-  amsterdamImage.onload = function () {
-    context.drawImage(amsterdamImage, 180, 35);
-    // Image Data Functions
-    // - createImageData() Creates a new, blank ImageData object
-    // - getImageData() Returns an ImageData object that copies the pixel data for the specified rectangle on a canvas
-    // - putImageData() Puts the image data (from a specified ImageData object) back onto the canvas
-
-    var imageData = context.getImageData(180, 35, 550, 366);
-
-    // R - The color red (from 0-255)
-    // G - The color green (from 0-255)
-    // B - The color blue (from 0-255)
-    // A - The alpha channel (from 0-255; 0 is transparent and 255 is fully visible)
-
-    for(var i = 0; i < imageData.data.length; i+=4){
-
-        var average = ( imageData.data[i] + imageData.data[i+1] + imageData.data[i+2]) / 3;
-        imageData.data[i] = average;       // Red
-        imageData.data[i+1] = average;     // Green
-        imageData.data[i+2] = average;     // Blue
-        //imageData.data[i+3] = 50;   // Alpha
-    }
-    context.putImageData(imageData, 180, 35);
+      // remove template entry
+      function templateRem() {
+          itmHeight = 0;
+          let templateNum = template.length;
+          if (templateNum > 0) {
+              ctx.clearRect(templateNum * 32, itmHeight * 32, 32, 32)
+          } else {
+              console.log('stop doing that');
+          }
+          template.pop();
+      }
 
 
-    // Save canvas image as data url ( default: PNG)
-    var dataURL = canvas.toDataURL();
+      // adding and removing template
+      const bttemplateNumAdd = document.querySelector('#templateAdd-btn');
+      bttemplateNumAdd.addEventListener('click', templateAdd);
+      const btnRemtemplate = document.querySelector('#templateRem-btn');
+      btnRemtemplate.addEventListener('click', templateRem);
 
-    document.getElementById('canvasImg').src = dataURL;
+      // key shortcut
+      document.addEventListener('keydown', (event) => {
+          event.preventDefault();
+          const keyPress = event.key;
 
+          if (keyPress === '1') {
+              templateAdd();
+              togAll();
+              attReset();
+          } else if (keyPress === '!') {
+              removetemplate();
+              return;
+          }
+      }, false);
 
-  };
-
-};
+  }
+  template();
